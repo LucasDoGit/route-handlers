234f77ccbf8ade4066c598586a82417655f1d087
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-10-08 11:55:00 -0300

    feat: estrutura de testes inicial

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import { Product } from "@/utils/types/produts";
+
+async function fetchProducts() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, {
+    next: { revalidate: 60 }, // Revalida a cada 60 segundos
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch products');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const products: Product[] = await fetchProducts();
+  return (
+    <div>
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>Preço: R$ {product.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
